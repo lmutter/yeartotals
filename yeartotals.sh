b1e0ef88af322c971b1524fe0424868392432276
Levi <41278493+lmutter@users.noreply.github.com>
2020-12-13 22:48:21 -0500

    Create yeartotals.sh

---
+++ yeartotals.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+itemType=Baby
+
+rm temp* 2> /dev/null
+awk -F "\"*,\"*" '{print $3 "," $6, "," $14}' records.csv >> temp
+cat temp | grep $itemType >> temp2
+
+for str in '2010' '2011' '2012' '2013' '2014' '2015' '2016' '2017'; do
+	echo "$str," | sed 's/201//' >> temp3
+	awk -v str="$str" -F "\"*,\"*" '
+	$2 ~ str {sum += $3} END {printf "%.12f\n", sum}
+	' temp2 >> $itemType.csv
+done
+rm temp*
